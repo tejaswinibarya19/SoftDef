d8007aeb56b3aa3434410779faf62de1c1001430
tejaswinibarya19 <tejaswinibarya19@gmail.com>
2026-05-07 23:24:19 +0530

    Add Navbar to App component

--- softdef/src/App.js
+++ softdef/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import React from 'react';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      Hello Tejaswini
+    <div className="min-h-screen bg-[#072702] text-white">
+        <div>
+          <Navbar/>
+        </div>
     </div>
   );
 }
