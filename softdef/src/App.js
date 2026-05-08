eb6e0206a1f9e43cfe5581242a3349e7e920b207
tejaswinibarya19 <tejaswinibarya19@gmail.com>
2026-05-08 15:14:47 +0530

    Upadte app layout with hero banner

--- softdef/src/App.js
+++ softdef/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import HeroBanner from './components/HeroBanner';
+import bgImage from './assests/Bg_image.png'
 function App() {
   return (
-    <div className="min-h-screen bg-[#072702] text-white">
-        <div>
-          <Navbar/>
-        </div>
+    <div className="min-h-screen w-full text-white bg-center bg-cover bg-no-repeat overflow-x-hidden"
+    style={{
+      background:`url(${bgImage})`,
+      backgroundSize:'cover'
+    }}>
+        
+      <Navbar/>
+      <HeroBanner/>
+        
     </div>
   );
 }
